4c3b356c152c26a99cfe91d2799d2eb4fee53ba9
Shaha Newaz <119354077+Shahanewaz007@users.noreply.github.com>
2023-11-25 11:44:01 +0600

    Add files via upload

---
+++ category/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=300, blank=True)
+    cat_images = models.ImageField(upload_to='photos/categories', blank=True)
+    
+    def __str__(self):
+        return self.category_name
+    
